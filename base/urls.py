bb139bca4813aa8db93e313dedf30b65f2dc0ab5
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-12-18 19:28:14 +0700

    Menambahkan rute ke views index

--- base/urls.py
+++ base/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def index(request):
-    return HttpResponse("Hallo semua")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gege/', index,name="index")
+    path('', include("api.urls"))
 ]
