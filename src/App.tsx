22b69419e0ba305534b8767462c96a508efc1dfe
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-12-21 22:30:45 +0700

    menambahkan src dan tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const reactLogo = "react.svg";
+const viteLogo = "vite.svg";
+
+const reactLogoPath = `/static/${reactLogo}`;
+const viteLogoPath = `/static/${viteLogo}`;
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
