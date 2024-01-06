e4d5ecccbf86fbae2c6dfdf49b2adaefb9a593ef
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2024-01-06 15:09:48 +0700

    membenarkan bug

--- src/pages/Contacts/components/FormInput.tsx
+++ src/pages/Contacts/components/FormInput.tsx
@@ -8,11 +8,10 @@ interface FormInputPropsType {
 }
 
 const FormInput: React.FC<FormInputPropsType> = ({ children, errors, t }) => {
-  const [value, setValue] = useState("");
   return (
     <div className="flex flex-col gap-y-4">
       <label htmlFor="fullname">{t(children)}</label>
-      <input type="text" name={children.toLowerCase()} id={children.toLowerCase()} className="rounded-md border-primary-700" value={value} onChange={(e) => setValue(e.target.value)} />
+      <input type="text" name={children.toLowerCase()} id={children.toLowerCase()} className="rounded-md border-primary-700" />
       {errors ? <p className="text-red-600">*{errors}</p> : null}
     </div>
   );
